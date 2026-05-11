7e0874a738541c3058820188f2c89be73bd5c00e
Gianfranco <92047251+GianfrancoMontiveroOK@users.noreply.github.com>
2026-05-11 12:43:16 -0300

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Box, useTheme } from "@mui/material";
+
 import Hero1 from "../components/Hero1";
 import Hero2 from "../components/Hero2";
 import Hero3 from "../components/Hero3";
@@ -7,28 +8,7 @@ import Hero4 from "../components/Hero4";
 import Hero5 from "../components/Hero5";
 import Hero6 from "../components/Hero6";
 import Hero7 from "../components/Hero7";
-import { GlobalStateContext } from "../App";
 
-export default function Homepage() {
-  const { power } = useContext(GlobalStateContext);
-  const theme = useTheme();
-
-  const backgroundColors = {
-    base: theme.palette.background.default,
-    kaio: "#0d1a1f",
-    mystic: "#1e0033",
-    ultraInstinct: "#111111",
-  };
-
-import React, { useContext } from "react";
-import { Box, useTheme } from "@mui/material";
-import Hero1 from "../components/Hero1";
-import Hero2 from "../components/Hero2";
-import Hero3 from "../components/Hero3";
-import Hero4 from "../components/Hero4";
-import Hero5 from "../components/Hero5";
-import Hero6 from "../components/Hero6";
-import Hero7 from "../components/Hero7";
 import { GlobalStateContext } from "../App";
 
 export default function Homepage() {
