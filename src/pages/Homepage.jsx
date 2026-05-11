de07fa9d77f974201826f9ca8136be736825a552
Gianfranco <92047251+GianfrancoMontiveroOK@users.noreply.github.com>
2026-05-11 12:39:53 -0300

    add

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -20,13 +20,34 @@ export default function Homepage() {
     ultraInstinct: "#111111",
   };
 
+import React, { useContext } from "react";
+import { Box, useTheme } from "@mui/material";
+import Hero1 from "../components/Hero1";
+import Hero2 from "../components/Hero2";
+import Hero3 from "../components/Hero3";
+import Hero4 from "../components/Hero4";
+import Hero5 from "../components/Hero5";
+import Hero6 from "../components/Hero6";
+import Hero7 from "../components/Hero7";
+import { GlobalStateContext } from "../App";
+
+export default function Homepage() {
+  const { power } = useContext(GlobalStateContext);
+  const theme = useTheme();
+
+  const backgroundColors = {
+    base: theme.palette.background.default,
+    kaio: "#0d1a1f",
+    mystic: "#1e0033",
+    ultraInstinct: "#111111",
+  };
+
   return (
     <Box
       sx={{
-        height: "100vh",
-        overflowY: "scroll",
-        scrollSnapType: "y mandatory",
-        scrollBehavior: "smooth",
+        width: "100%",
+        minHeight: "100vh",
+        overflowX: "hidden",
         backgroundColor:
           backgroundColors[power] || theme.palette.background.default,
         transition: "background-color 0.5s ease",
